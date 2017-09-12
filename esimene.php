205c6e33cb3753f4cc87751699bcac6ba8c1f8b9
Dennis Richard Šulga <desulg@users.noreply.github.com>
2017-09-12 14:15:37 +0300

    Lisatud on esimene jupp PHP koodi

---
+++ esimene.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Dennis Richard Šulga veebiprogrammeerimine</title>
+</head>
+<body>
+	<h1>Dennise veebiproge</h1>
+	<p>See leht on loodud õppetöö raames ja siit otsida midagi on mõttetu.</p>
+	<h2>Dennise tutvustus</h2>
+	<p>Aga no kui sa juba siia sattusid, siis räägin paar hästi olulist fakti enda kohta:</p>
+		<ul>
+			<li>Korvpall on elu</li>
+			<li>Olen kontsert aka live valgustaja</li>
+			<li>Elu on liiga lühike, et otsida sahtlist sarnaseid sokke</li>
+			<li>Tegelen muusikaga ka ja mängin bassi</li>
+		</ul>
+	<p><i>P.S. Tahaks siia pilte ka lisada. Varsti tulevad</i><p> 
+	
+	<?php
+		echo "See on esimene jupp PHP abil väljastatud teksti!";
+	?>
+	
+</body>	
+</html>
