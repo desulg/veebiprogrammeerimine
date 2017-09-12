78c1870c2ee8eb02ec637abd20cbb0ece67685aa
Dennis Richard Šulga <desulg@users.noreply.github.com>
2017-09-12 15:29:32 +0300

    Lisatud muutujad ja if laused

--- esimene.php
+++ esimene.php
@@ -1,3 +1,32 @@
+<?php
+	//See on kommentaar, järgmisena paar muutujat
+	$myName = "Dennis Richard";
+	$myFamilyName = "Šulga";
+	//vaatame, mis kell on ja määramae päeva osa
+	$hourNow = date("H");
+	//echo $hourNow;
+	$partOfDay = "";
+	if ( $hourNow < 8){
+		$partOfDay = "varajane hommik";
+	}
+	if ($hourNow >= 8 and $hourNow < 16){
+		$partOfDay = "koolipäev";
+	}
+	if ($hourNow >16){
+		$partOfDay = "vaba aeg";
+	}
+	//echo $partOfDay;
+	
+	//vaatame, kaua on kooliüäeva lõpuni aega jäänud
+	$timeNow = strtotime(date("d.m.Y H:i:s"));
+	echo $timeNow;
+	$schoolDayEnd = strtotime(date("d.m.Y" ." " ."15:45")); 
+	//echo $schoolDayEnd;
+	$toTheEnd = $schoolDayEnd - $timeNow;
+	echo "Koolipäeva lõpuni on jäänud ". round($toTheEnd / 60);
+?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,7 +34,11 @@
 	<title>Dennis Richard Šulga veebiprogrammeerimine</title>
 </head>
 <body>
-	<h1>Dennise veebiproge</h1>
+	<h1>
+	<?php
+		echo $myName ." ".$myFamilyName
+	?>
+	 veebiproge</h1>
 	<p>See leht on loodud õppetöö raames ja siit otsida midagi on mõttetu.</p>
 	<h2>Dennise tutvustus</h2>
 	<p>Aga no kui sa juba siia sattusid, siis räägin paar hästi olulist fakti enda kohta:</p>
@@ -18,7 +51,10 @@
 	<p><i>P.S. Tahaks siia pilte ka lisada. Varsti tulevad</i><p> 
 	
 	<?php
-		echo "See on esimene jupp PHP abil väljastatud teksti!";
+		echo "<p>See on esimene jupp PHP abil väljastatud teksti!</p>";
+		echo "<p>Täna on ";
+		echo date("d.m.Y") .", kell lehe avamisel oli " .date("H:i:s");
+		echo ", käes on ". $partOfDay.".</p>";
 	?>
 	
 </body>	
